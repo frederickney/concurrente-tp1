c2caddc26f5ac952471e8042defcb11b20399f6d
frederick ney <frederick.ney@gmail.com>
2015-10-06 16:44:54 +0200

    Adding macro fors extending input file

--- src/convolution.c
+++ src/convolution.c
@@ -5,9 +5,31 @@
 \****************************************************************************/
 
 #include "convolution.h"
+#include "../lib/ppm.h"
+#include "filter.h"
 
+void extend_3(img_t *input , img_t *ouput)
+{
+
+}
+
+void extend_5(img_t *input , img_t *ouput)
+{
+
+}
 
 void convolution (img_t *output, const img_t *input, const filter_t *filter, int threads = 0)
 {
+    int width = input->width;
+    int height =  input->height;
+    int dim = filter_t->dim;
+    img_t extend_input;
+
+    if (dim == 3) goto dim_3;
+    else if (dim == 5) goto dim_5;
+
+dim_3:
+
+dim_5:
 
 }
