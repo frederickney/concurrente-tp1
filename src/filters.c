416959b8d3bf9e2420cc5dc14e4ec3a0b9bf2002
frederick ney <frederick.ney@gmail.com>
2015-10-06 13:26:07 +0200

    changing initialisation of structure and the filter array

--- src/filters.c
+++ src/filters.c
@@ -9,109 +9,104 @@
 /*
  * Global constant for the identity filter
  */
-const int IDENTITY_SIZE = 3;
-const float IDENTITY_KERNEL[IDENTITY_SIZE][IDENTITY_SIZE] = { {0, 0, 0}, {0, 1, 0}, {0, 0, 0} };
-/*
- * Global constant for the sharpen filter
- */
-const int SHARPEN_SIZE = 3;
-const float SHARPEN_KERNEL[SHARPEN_SIZE][SHARPEN_SIZE] = { {0, -1, 0}, {-1, 5, -1}, {0, -1, 0} };
-/*
- * Global constant for the blur filter
- */
-const int BLUR_SIZE = 3;
-const float BLUR_KERNEL[BLUR_SIZE][BLUR_SIZE] = { [0 ... 2][0 ... 2] = ((float) 1) / ((float) 9) };
-/*
- * Global constant for the edge filter
- */
-const int EDGE_SIZE = 3;
-const float EDGE_KERNEL[EDGE_SIZE][EDGE_SIZE] = { {-1, -1, -1}, {-1, 8, -1}, {-1, -1, -1} };
-/*
- * Global constant for the gauss filter
- */
-const float UNDIV = 256; // unsigned dividend value
-const int GAUSS_SIZE = 5;
-const float GAUSS_KERNEL[gauss_size][gauss_size] = {
-  {((float) 1) / UNDIV, ((float) 4) / UNDIV, ((float) 6) / UNDIV, ((float) 4) / UNDIV, ((float) 1) / UNDIV},
-  {((float) 4) / UNDIV, ((float) 16) / UNDIV, ((float) 24) / UNDIV, ((float) 16) / UNDIV, ((float) 4) / UNDIV},
-  {((float) 16) / UNDIV, ((float) 24) / UNDIV, ((float) 36) / UNDIV, ((float) 24) / UNDIV, ((float) 16) / UNDIV},
-  {((float) 4) / UNDIV, ((float) 16) / UNDIV, ((float) 24) / UNDIV, ((float) 16) / UNDIV, ((float) 4) / UNDIV},
-  {((float) 1) / UNDIV, ((float) 4) / UNDIV, ((float) 6) / UNDIV, ((float) 4) / UNDIV, ((float) 1) / UNDIV}
-};
-/*
- * Global constant for the unsharp filter
- */
-float DIV = -256;
-int UNSHARP_SIZE = 5;
-int UNSHARP_KERNEL[UNSHARP_SIZE][UNSHARP_SIZE] = {
-  {((float) 1) / DIV, ((float) 4) / DIV, ((float) 6) / DIV, ((float) 4) / DIV, ((float) 1) / DIV},
-  {((float) 4) / DIV, ((float) 16) / DIV, ((float) 24) / DIV, ((float) 16) / DIV, ((float) 4) / DIV},
-  {((float) 16) / DIV, ((float) 24) / DIV, ((float) 36) / DIV, ((float) 24) / DIV, ((float) 16) / DIV},
-  {((float) 4) / DIV, ((float) 16) / DIV, ((float) 24) / DIV, ((float) 16) / DIV, ((float) 4) / DIV},
-  {((float) 1) / DIV, ((float) 4) / DIV, ((float) 6) / DIV, ((float) 4) / DIV, ((float) 1) / DIV}
-};
-/*
 const float IDENTITY_KERNEL[] = {
   0, 0, 0,
   0, 1, 0,
   0, 0, 0
 };
+/*
+ * Global constant for the sharpen filter
+ */
+const float SHARPEN_KERNEL[] = {
+  0, -1, 0,
+  -1, 5, -1,
+  0, -1, 0
+};
+/*
+ * Global constant for the blur filter
+ */
+const float BLUR_KERNEL[] = { [0 ... 8] = 1.0 / 9.0 };
+/*
+ * Global constant for the edge filter
+ */
+const float EDGE_KERNEL[]] = {
+  -1, -1, -1,
+  -1, 8, -1,
+  -1, -1, -1
+};
+/*
+ * Global constant for the gauss filter
+ */
+const float UNDIV = 256.0; // unsigned dividend value
+const float GAUSS_KERNEL[] = {
+  1.0 / UNDIV, 4.0 / UNDIV, 6.0 / UNDIV, 4.0 / UNDIV, 1.0 / UNDIV,
+  4.0 / UNDIV, 16.0 / UNDIV, 24.0 / UNDIV, 16.0 / UNDIV, 4.0 / UNDIV,
+  16.0 / UNDIV, 24.0 / UNDIV, 36.0 / UNDIV, 24.0 / UNDIV, 16.0 / UNDIV,
+  4.0 / UNDIV, 16.0 / UNDIV, 24.0 / UNDIV, 16.0 / UNDIV, 4.0 / UNDIV,
+  1.0 / UNDIV, 4.0 / UNDIV, 6.0 / UNDIV, 4.0 / UNDIV, 1.0 / UNDIV
+};
+/*
+ * Global constant for the unsharp filter
+ */
+float DIV = -256.0;
+int UNSHARP_KERNEL[] = {
+  1.0 / DIV, 4.0 / DIV, 6.0 / DIV, 4.0 / DIV, 1.0 / DIV,
+  4.0 / DIV, 16.0 / DIV, 24.0 / DIV, 16.0 / DIV, 4.0 / DIV,
+  16.0 / DIV, 24.0 / DIV, 36.0 / DIV, 24.0 / DIV, 16.0 / DIV,
+  4.0 / DIV, 16.0 / DIV, 24.0 / DIV, 16.0 / DIV, 4.0 / DIV,
+  1.0 / DIV, 4.0 / DIV, 6.0 / DIV, 4.0 / DIV, 1.0 / DIV
+};
+
 const filter_t IDENTITY_FILTER = {
   .name = "identity",
   .dim = 3,
   .kernel = IDENTITY_KERNEL
-};*/
+};
+
+const filter_t SHARPEN_FILTER = {
+  .name = "sharpen",
+  .dim = 3,
+  .kernel = SHARPEN_KERNEL
+}
+
+const filter_t BLUR_FILTER = {
+  .name = "blur",
+  .dim = 3,
+  .kernel = BLUR_KERNEL
+}
+
+const filter_t EDGE_FILTER = {
+  .name = "edge",
+  .dim = 3,
+  .kernel = EDGE_KERNEL
+}
+
+const filter_t GAUSS_FILTER = {
+  .name = "gauss",
+  .dim = 5,
+  .kernel = GAUSS_KERNEL
+}
+
+const filter_t UNSHARP_FILTER = {
+  .name = "unsharp",
+  .dim = 5,
+  .kernel = UNSHARP_KERNEL
+}
 
 const filter_t *filters_get_by_name(const char *name)
 {
-  if (strcmp(name, "identity") == 0)
-    {
-      filter_t IDENTITY_FILTER;
-      IDENTITY_FILTER->name = name;
-      IDENTITY_FILTER->dim = IDENTITY_SIZE;
-      IDENTITY_FILTER->kernel = &IDENTITY_KERNEL;
+  if (strcmp(name, IDENTITY_FILTER->name) == 0)
       return &IDENTITY_FILTER;
-    }
-    else if (strcmp(name, "sharpen") == 0)
-    {
-      filter_t SHARPEN_FILTER;
-      SHARPEN_FILTER->name = name;
-      SHARPEN_FILTER->dim = SHARPEN_SIZE;
-      SHARPEN_FILTER->kernel = &SHARPEN_KERNEL;
+    else if (strcmp(name, SHARPEN_FILTER) == 0)
       return &SHARPEN_FILTER;
-    }
-    else if (strcmp(name, "blur") == 0)
-    {
-      filter_t BLUR_FILTER;
-      BLUR_FILTER->name = name;
-      BLUR_FILTER->dim = BLUR_SIZE;
-      BLUR_FILTER->kernel = &BLUR_KERNEL;
+    else if (strcmp(name, BLUR_FILTER->name) == 0)
       return &BLUR_FILTER;
-    }
-    else if (strcmp(name, "edge") == 0)
-    {
-      filter_t EDGE_FILTER;
-      EDGE_FILTER->name = name;
-      EDGE_FILTER->dim = EDGE_SIZE;
-      EDGE_FILTER->kernel = &EDGE_KERNEL;
+    else if (strcmp(name, EDGE_FILTER->name) == 0)
       return &EDGE_FILTER;
-    }
-    else if (strcmp(name, "gauss") == 0)
-    {
-      filter_t GAUSS_FILTER;
-      GAUSS_FILTER->name = name;
-      GAUSS_FILTER->dim = GAUSS_SIZE;
-      GAUSS_FILTER->kernel = &GAUSS_KERNEL;
+    else if (strcmp(name, GAUSS_FILTER->name) == 0)
       return &GAUSS_FILTER;
-    }
-    else if (strcmp(name, "unsharp") == 0)
-    {
-      filter_t UNSHARP_FILTER;
-      UNSHARP_FILTER->name = name;
-      UNSHARP_FILTER->dim = UNSHARP_SIZE;
-      UNSHARP_FILTER->kernel = &UNSHARP_KERNEL;
+    else if (strcmp(name, UNSHARP_FILTER->name) == 0)
       return &UNSHARP_FILTER;
-    }
   else
     return NULL;
 }
