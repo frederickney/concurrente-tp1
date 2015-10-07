80820131ee203afdc2e9e0c1de735c6ff2931d49
frederick ney <frederick.ney@gmail.com>
2015-10-07 19:14:01 +0200

    tab changed

--- src/convolution.c
+++ src/convolution.c
@@ -9,16 +9,17 @@
 
 void convolution(img_t *output, const img_t *input, const filter_t *filter, int threads)
 {
-    int width = input->width;
-    int height =  input->height;
-    int address;
+  int width = input->width;
+  int height =  input->height;
+  int address;
 
-    for(int y = 0; y < height; y++) {
-      for (int x = 0; x < width; x++) {
-        address = width * y + x;
-        output->data[address].g = input->data[address].g;
-        output->data[address].r = input->data[address].r;
-        output->data[address].b = input->data[address].b;
-      }
+  for(int y = 0; y < height; y++) {
+    for (int x = 0; x < width; x++) {
+      address = width * y + x;
+      output->data[address].g = input->data[address].g;
+      output->data[address].r = input->data[address].r;
+      output->data[address].b = input->data[address].b;
     }
+  }
 }
+
