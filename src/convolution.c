9475b001eb188dacfbf78d1a824fe6db0d32f977
frederick ney <frederick.ney@gmail.com>
2015-10-07 20:02:23 +0200

    add mono-threaded convolution

--- src/convolution.c
+++ src/convolution.c
@@ -12,14 +12,30 @@ void convolution(img_t *output, const img_t *input, const filter_t *filter, int 
   int width = input->width;
   int height =  input->height;
   int address;
+  int half_dim = (filter->dim-1)/2;
+  int neighbor;
+  float coef;
+  float r, g, b;
 
   for(int y = 0; y < height; y++) {
     for (int x = 0; x < width; x++) {
       address = width * y + x;
-      output->data[address].g = input->data[address].g;
-      output->data[address].r = input->data[address].r;
-      output->data[address].b = input->data[address].b;
+      r = g = b = 0;
+
+      for (int xk = -half_dim; xk <= half_dim; xk++){
+        for (int yk = -half_dim; yk <= half_dim; yk++)
+        {
+          coef = filter_get(filter, half_dim + xk, half_dim + yk);
+          neighbor = width * clamp(y + yk, 0 , height) +clamp(x + xk, 0, width);
+          g += coef * input->data[neighbor].g;
+          r += coef * input->data[neighbor].r;
+          b += coef * input->data[neighbor].b;
+        }
+      }
+
+      output->data[address].r = (uint8_t) clamp(r, 0, 255);
+      output->data[address].g = (uint8_t) clamp(g, 0, 255);
+      output->data[address].b = (uint8_t) clamp(b, 0, 255);
     }
   }
 }
-
