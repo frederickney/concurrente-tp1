dd155b6e8d4c84b2c898e64fa164afe550724446
frederick ney <frederick.ney@gmail.com>
2015-10-07 12:28:14 +0200

    extension of input picture for all filter finished

--- src/convolution.c
+++ src/convolution.c
@@ -18,33 +18,23 @@ img_t *extend(img_t *input)
     for (int i = 0; i < extend->width * extend->height; i++)
     {
         if (i == 0)
-            extend->data[i] = input->data[i];   //building the left corner extension pixel
+            extend->data[i] = input->data[i];   //creating the left corner extension pixel
         else if (i == w_ext)
-            extend->data[i] = input->data[i-2]; //building the right corner extension pixel
+            extend->data[i] = input->data[i-2]; //creating the right corner extension pixel
         else if ( i > 0 && i < width)
-            extend->data[i] = input->data[i - 1];   //building the rest of the 1st pixel line extension
-        else if (i % w_ext == 1 && i / w_ext > 0 && i / w_ext < h_ext)  // detecting the begin of a pixel line
+            extend->data[i] = input->data[i - 1];   //creating the rest of the 1st pixels line extension
+        else if (i % w_ext == 1 && i / w_ext > 0 && i / w_ext < h_ext)  // detecting the begin of a pixels line
             extend->data[i] = input->data[(i / w_ext - 1) * (width - 1) + 1];   //left extension of input picture
-        else if (i % w_ext == 0 && i / w_ext > 0 && i / w_ext < h_ext)  //detecting the end of a pixel line
+        else if (i % w_ext == 0 && i / w_ext > 0 && i / w_ext < h_ext)  //detecting the end of a pixels line
             extend->data[i] = input->data[(i / w_ext - 1) * (width - 1)];    //right extension of input picture
         else if (i%w_ext > 0 && i%w_ext < w_ext && i/w_ext > 0 && i/w_ext < h_ext) //place to insert input picture
             extend->data[i] = input->data[(i / w_ext - 1) * (width - 1) + i % w_ext];   //inserting the picture
-        else if (i > height * w_ext)    //building the last pixel line of extend
-            extend->data[i] = extend->data[i - w_ext];  //copy of previous pixel line into last pixel line
+        else if (i > height * w_ext)    //creating the last pixel line of extend
+            extend->data[i] = extend->data[i - w_ext];  //copy of previous pixels line into last pixels line
     }
     return extend;
 }
-/*
-img_t *extend_5(img_t *input)
-{
-    img_t *extend = alloc_img(input->width + 4 , input->height + 4);
-    int w_ext = input->width + 1;
-    int h_ext = input->height + 1;
-    int height = input->height;
-    int width = input->width;
-    return extend;
-}
-*/
+
 void convolution (img_t *output, const img_t *input, const filter_t *filter, int threads = 0)
 {
     int width = input->width;
