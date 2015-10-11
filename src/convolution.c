78aaaa38e6b56698539039403c97fdda2b44f3a7
frederick ney <frederick.ney@gmail.com>
2015-10-11 15:09:10 +0200

    segmentation fault bug corrected

--- src/convolution.c
+++ src/convolution.c
@@ -21,7 +21,7 @@ void *thread(void *argv)
   int address, neighbor;
   float coef, r, g, b;
 
-  for(int y = begin * width; y < end * width; y++) {
+  for(int y = begin; y < end; y++) {
     for (int x = 0; x < width; x++) {
       address = width * y + x;
       r = g = b = 0;
@@ -30,12 +30,11 @@ void *thread(void *argv)
         for (int yk = -half_dim; yk <= half_dim; yk++) {
           coef = filter_get(data->filter, half_dim + xk, half_dim + yk);
           neighbor = width * clamp(y + yk, 0 , height) +clamp(x + xk, 0, width);
-          g += coef * data->input->data[neighbor].r;
-          r += coef * data->input->data[neighbor].g;
+          r += coef * data->input->data[neighbor].r;
+          g += coef * data->input->data[neighbor].g;
           b += coef * data->input->data[neighbor].b;
         }
       }
-
       data->output->data[address].r = (uint8_t) clamp(r, 0, 255);
       data->output->data[address].g = (uint8_t) clamp(g, 0, 255);
       data->output->data[address].b = (uint8_t) clamp(b, 0, 255);
