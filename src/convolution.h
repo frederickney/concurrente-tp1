9a04db4ac3bf82180cab0fefa00707256ea2de3b
frederick ney <frederick.ney@gmail.com>
2015-10-11 10:16:15 +0200

    creating structure for sending data on threads functions

--- src/convolution.h
+++ src/convolution.h
@@ -13,8 +13,13 @@
 
 #define clamp(x , min , max) (((min) < (x)) ? (((x) < (max)) ? (x) : (max)) : (min))
 
-/**
- *
- */
+typedef struct {
+  const int begin, end;
+  const filter_t *filter;
+  const img_t *input;
+  img_t *output;
+} data_t;
+
 void convolution (img_t *output, const img_t *input, const filter_t *filter , int threads);
+void threads (void * argv);
 #endif // CONVOLUTION_H
