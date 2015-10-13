25708c7f1f7110febdf65805c8a48acce8c9e6bd
Vincent Berset <vberset@protonmail.ch>
2015-10-13 21:14:38 +0200

    Add convolution's documentation

--- src/convolution.h
+++ src/convolution.h
@@ -1,9 +1,6 @@
 /****************************************************************************\
- * @brief
- *
- *
+ * @brief Convolution's computation and parallelization - header
 \****************************************************************************/
-
 #ifndef CONVOLUTION_H
 #define CONVOLUTION_H
 
@@ -11,15 +8,30 @@
 
 #include "filters.h"
 
+/**
+ * @brief Restrict the `x` value to the given range `[min:max]`
+ */
 #define clamp(x, min, max) (((min) < (x)) ? (((x) < (max)) ? (x) : (max)) : (min))
 
+/**
+ * Thread's computation arguments
+ */
 typedef struct {
-  int begin, end;
-  const filter_t *filter;
-  const img_t *input;
-  img_t *output;
+  int begin;              // First row to compute
+  int end;                // Last row to compute
+  const filter_t *filter; // Filter to apply
+  const img_t *input;     // Input image
+  img_t *output;          // Output image
 } data_t;
 
+/**
+ * @brief Parallelize convolution's omputation.
+ *
+ * @param output Output image
+ * @param input Input image
+ * @param filter Fitler to apply
+ * @param num_threads Number of threads to use
+ */
 void convolution (
   img_t *const output,
   const img_t *const input,
@@ -27,5 +39,10 @@ void convolution (
   const int num_threads
 );
 
+/**
+ * @brief Compute the convolution on the specified image tile.
+ *
+ * @param args Computation arguments
+ */
 void *thread (void *args);
 #endif // CONVOLUTION_H
